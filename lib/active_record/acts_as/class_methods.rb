92c4fffa67bbad304f9f4e55d1fa0fd2de7118af
nankrah <nankrah@weddingwire.com>
2015-12-10 23:39:08 -0500

    remove acts_as name from child reflections

--- lib/active_record/acts_as/class_methods.rb
+++ lib/active_record/acts_as/class_methods.rb
@@ -5,9 +5,26 @@ module ActiveRecord
         module_.alias_method_chain :_reflections, :acts_as
       end
 
+
+
       def _reflections_with_acts_as
+
+         puts '######START#####'
+         puts "reflections without acts as: #{_reflections_without_acts_as.keys.join(', ')}"
+
+         puts "acting_as_model: #{acting_as_name}"
+         puts "acting model reflections:  #{acting_as_model._reflections.keys.join(', ')}"
+         puts '###END####'
+         puts "\r\r\r"
         @_reflections_acts_as_cache ||=
           _reflections_without_acts_as.reverse_merge(acting_as_model._reflections)
+
+         @_reflections_acts_as_cache.delete_if{|k,v| puts "KEY:#{k}"; k.to_s == acting_as_name}
+
+         puts "#{@_reflections_acts_as_cache.keys}"
+         puts '###END####'
+         puts "\r\r\r"
+         @_reflections_acts_as_cache
       end
 
       def validators_on(*args)
